a1f8b7213d169f0acc66115d93dd022423913a84
ramchepuri <ramchander.chepuri@gmail.com>
2018-04-05 18:59:13 -0400

    HomeWork resubmission

---
+++ NewsMoodPy/config.py
@@ -0,0 +1,4 @@
+consumer_key = 'koOOuBKRiItHvASVuQaadOJXT'
+consumer_secret = '2znO6mK5RzTpZNRfiLatSryokrEl95wqMrW9TizTosTyrc3SI0'
+access_token = '146304109-PjYC9RXTi9fhCNlRKQiwxhnSyEnqcYx2JaYz8SPF'
+access_token_secret = 'rlFn9tQlswR1vkKpeK2o10g0fk9dnPQXywFEJZUbNN7Vs'
